b46ab4dad6bcd4bf5267f0f03a91566eaaff9429
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-05-04 16:53:15 -0300

    insert folder _img

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 	<div class="container">
 
 		<div class="py-3 text-center">
-			<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+			<img class="d-block mx-auto mb-2" src="./_img/logo.png" alt="" width="72" height="72">
 			<h2>Send Mail</h2>
 			<p class="lead">Seu app de envio de e-mails particular!</p>
 		</div>

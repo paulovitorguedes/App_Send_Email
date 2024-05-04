9db550ff8655541a79f50ad06be253647de8de53
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-05-04 18:03:51 -0300

    class Email

---
+++ _class/email.php
@@ -0,0 +1,22 @@
+<?php
+
+class Email{
+
+    private $email = "";
+    private $subject = "";
+    private $message = "";
+
+    function __construct($email, $subject, $message){
+        $this->__set('$email', $email);
+        $this->__set('$subject', $subject);
+        $this->__set('$message', $message);
+    }
+
+    public function __get($attr){
+        return $this->$attr;
+    }
+    public function __set($attr, $value){
+        $this->$attr = $value;
+    }
+
+}
--- _module/indexModu.php
+++ _module/indexModu.php
@@ -1,11 +1,18 @@
 <?php
 
+require_once "../_class/email.php";
+
+
+
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 $subject = filter_input(INPUT_POST, 'subject', FILTER_DEFAULT);
 $message = filter_input(INPUT_POST, 'message', FILTER_DEFAULT);
 
+
+
 if($email && !empty($subject) && !empty($message)) {
-    echo "mensagem válida";
+    $mailing = new Email($email, $subject, $message);
+    echo var_dump($mailing);
 } else {
     echo "Falhou";
 }

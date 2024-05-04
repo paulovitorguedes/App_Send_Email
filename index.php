d5c7b8f628d3b09337ea359167bc9c4e3ce6b6b0
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-05-04 16:42:29 -0300

    insert index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<!-- Meta tags Obrigatórias -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="Paulo Vitor Guedes">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+		integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<title>App Mail Send</title>
+
+</head>
+
+<body>
+
+	<div class="container">
+
+		<div class="py-3 text-center">
+			<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+			<h2>Send Mail</h2>
+			<p class="lead">Seu app de envio de e-mails particular!</p>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+
+				<div class="card-body font-weight-bold">
+					<form>
+						<div class="form-group">
+							<label for="para">Para</label>
+							<input type="text" class="form-control" id="para" placeholder="@dominio.com.br">
+						</div>
+
+						<div class="form-group">
+							<label for="assunto">Assunto</label>
+							<input type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+						</div>
+
+						<div class="form-group">
+							<label for="mensagem">Mensagem</label>
+							<textarea class="form-control" id="mensagem"></textarea>
+						</div>
+
+						<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</>
